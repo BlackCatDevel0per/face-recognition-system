1a81d184d1bbb620a710f36d5e43467e8482add5
BlackCatDevel0per <333blackcat333@mail.ru>
2022-04-08 15:04:22 +0500

    Update file

--- main/models.py
+++ main/models.py
@@ -1,10 +1,10 @@
-from django.db import models
-import os
+#import os
 import uuid
+from django.db import models
 
 # Create your models here.
 
-def make_image(instanse,filename):
+def make_image(instanse, filename):
     name = instanse.name
     id_ = instanse.uuid
     file_format = filename.split('.')[-1]
@@ -17,13 +17,13 @@ class Student(models.Model):
     surname = models.CharField('Familyasi', max_length=25)
     birth_date = models.DateField('Tugulgan kuni', blank=True, null=True)
     address = models.CharField('Manzili', max_length=355)
-    phone = models.CharField('Telefoni', max_length=16,blank=True)
-    parents_phone = models.CharField('Qarindoshining telefoni', max_length=16,blank=True)
-    parents_info = models.CharField('Ismi ', max_length=16,blank=True)
-    parents_status = models.CharField('Qarindoshligi', max_length=16,blank=True)
+    phone = models.CharField('Telefoni', max_length=16, blank=True)
+    parents_phone = models.CharField('Qarindoshining telefoni', max_length=16, blank=True)
+    parents_info = models.CharField('Ismi ', max_length=16, blank=True)
+    parents_status = models.CharField('Qarindoshligi', max_length=16, blank=True)
     #uuid = models.UUIDField(max_length=150, blank=True,null=True)
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    discount = models.PositiveIntegerField("Chegirma",default=0)
+    discount = models.PositiveIntegerField("Chegirma", default=0)
     debt = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to=make_image) # image not deleting!!!
 
